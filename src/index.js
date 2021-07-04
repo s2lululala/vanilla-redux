511ba8bcc9c82e6e73392f87d97806f8975a71f1
Soohyun Choi <djm05190@gmail.com>
2021-07-04 22:50:10 +0900

    1.3 subscriptions

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+	console.log(count, action);
+	if (action.type === "ADD") {
+		return count + 1;
+	} else if (action.type === "MINUS") {
+		return count -1;
+	} else {
+		return count;
+	}
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+	number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({type: "ADD"}));
+minus.addEventListener("click", () => countStore.dispatch({type: "MINUS"}));
